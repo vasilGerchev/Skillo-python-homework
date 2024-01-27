c4909505e49e734047c5957f692d6fe7e45e2a0d
vasilGerchev <vasil_a_g@hotmail.com>
2024-01-27 16:12:51 +0200

    Initial commit

---
+++ homework/example.py
@@ -0,0 +1,9 @@
+name_one = ("Ivan Ivanov, PIN:", 8410111234)
+name_two = ("Petar Stoianov, PIN:", 9011134567)
+name_three = ("Atanas Petrov, PIN:", 8009118970)
+name_four = ("Stoian Stoianov, PIN:", 9305129076)
+name_five = ("Aleksandar Georgiev, PIN:", 7603051478)
+
+print(name_one)
+print(name_three)
+print(name_five)
