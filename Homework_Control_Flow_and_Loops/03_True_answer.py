0132bd24bbb05474b780fdb3bf2f0ada383ce377
vasilGerchev <vasil_a_g@hotmail.com>
2024-01-28 10:15:16 +0200

    True answer and number divisible

---
+++ Homework_Control_Flow_and_Loops/03_True_answer.py
@@ -0,0 +1,10 @@
+answer = int(input("Please answer a question - how much does 5 + 17 equal to: "))
+
+while answer != 22:
+    answer = int(input("Wrong answer, please try again:  "))
+if answer == 22:
+    print("Congratulation, True answer")
+
+
+
+
---
+++ Homework_Control_Flow_and_Loops/Numbers_divisible_3_and_5.py
@@ -0,0 +1,6 @@
+count = 1
+
+while count < 1000:
+    count += 1
+    if (count % 3) == 0:
+        print(count)
