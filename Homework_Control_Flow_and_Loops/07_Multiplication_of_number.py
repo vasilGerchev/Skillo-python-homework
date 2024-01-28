15c40290deead946485ae8e03ece1b20d2476858
vasilGerchev <vasil_a_g@hotmail.com>
2024-01-28 18:55:19 +0200

    multiplication of number

---
+++ Homework_Control_Flow_and_Loops/07_Multiplication_of_number.py
@@ -0,0 +1,5 @@
+number = int(input("Please enter the number: "))
+listOne = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for multiplicationList in listOne:
+    result = number * multiplicationList
+    print(f"{number} * {multiplicationList} = {result}")
